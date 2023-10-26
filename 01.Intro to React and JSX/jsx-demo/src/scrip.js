6f556123a3f433f62fc8e0f3e5ffc3af9720961b
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-10-26 09:22:46 +0300

    new update

---
+++ 01.Intro to React and JSX/jsx-demo/src/scrip.js
@@ -0,0 +1,5 @@
+import '../node_modules/react/umd/react.production.min.js'
+import '../node_modules/react-dom/umd/react-dom.production.min.js'
+
+const rootElement = document.createElement('root');
+
