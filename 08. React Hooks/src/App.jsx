a9863713370e197d7e4d9c0d5f4fe69a7ee6a9de
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-11-22 18:44:10 +0200

    React hooks and routing, new workshop

---
+++ 08. React Hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header';
+import ToDoList from './ToDoList';
+
+function App() {
+    return (
+        <>
+         <Header />
+         <ToDoList />
+        </>
+    )
+}
+
+export default App
